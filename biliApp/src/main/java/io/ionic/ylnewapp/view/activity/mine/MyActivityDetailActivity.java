9804c5efd6842d87b8645ba95b2450d69d7de497
Lavned <1464208707@qq.com>
2018-08-03 16:59:57 +0800

    第三版修改

---
+++ biliApp/src/main/java/io/ionic/ylnewapp/view/activity/mine/MyActivityDetailActivity.java
@@ -0,0 +1,147 @@
+package io.ionic.ylnewapp.view.activity.mine;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jaeger.library.StatusBarUtil;
+import com.umeng.analytics.MobclickAgent;
+
+import org.xutils.view.annotation.ViewInject;
+
+import io.ionic.ylnewapp.R;
+import io.ionic.ylnewapp.utils.PreferenceUtils;
+import io.ionic.ylnewapp.view.base.BaseActivity;
+
+public class MyActivityDetailActivity extends BaseActivity {
+
+    @ViewInject(R.id.all_webview)
+    WebView myWebView;
+    @ViewInject(R.id.finshs)
+    TextView finshs;
+
+    String url="";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        StatusBarUtil.setColor(this,getColor(R.color.colorPrimary),225);
+        init();
+        Intent intent = getIntent();
+        url = intent.getStringExtra("url");
+        String action = intent.getAction();
+        loadView(url+"?username="+PreferenceUtils.getPrefString(mContext,"account",""));
+        if(Intent.ACTION_VIEW.equals(action)){
+            Uri uri = intent.getData();
+            if(uri != null){
+                String id = uri.getQueryParameter("id");
+                Toast.makeText(this,id,Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    //退出事件
+    private void init() {
+        mBuilder.setTitle("加载中...").show();
+        finshs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyActiActivity.activity.finish();
+                finish();
+            }
+        });
+    }
+
+
+    /**
+     * a加载网页
+     * @param url
+     */
+    private void loadView(String url) {
+        //要加载的H5页面
+        myWebView.loadUrl(url);
+        WebSettings settings = myWebView.getSettings();
+        settings.setDomStorageEnabled(true);
+        settings.setJavaScriptEnabled(true);
+        //将WebAppInterface于javascript绑定
+        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        //不读取缓存
+//        myWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+        myWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                String scheme = Uri.parse(url).getScheme();//还需要判断host
+                if (TextUtils.equals("myapp", scheme)) {
+                    PreferenceUtils.setPrefString(mContext,"item","1");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                    finish();
+                    return true;
+                }
+                if (TextUtils.equals("coupons", scheme)) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                    MyActiActivity.activity.finish();
+                    finish();
+                    return true;
+                }
+                return false;
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                mBuilder.dismiss();
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                mBuilder.setTitle("加载中").show();
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    public class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /** Show a toast from the web page */
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
